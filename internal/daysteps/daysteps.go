4535aa8bd576a9c3d2b68b9106bfd4ca3fcdc5fa
Чернухин <maddy08@inbox.ru>
2025-07-31 17:28:19 +0300

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,12 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -13,8 +18,58 @@ const (
 
 func parsePackage(data string) (int, time.Duration, error) {
 	// TODO: реализовать функцию
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, fmt.Errorf("неверный формат данных: ожидалось 'шаги,продолжительность', получено '%s'", data)
+	}
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+
+		return 0, 0, fmt.Errorf("не удалось преобразовать шаги в число: %w", err)
+	}
+
+	//Количество шагов должно быть больше 0.
+	if steps <= 0 {
+		return 0, 0, fmt.Errorf("количество шагов должно быть положительным числом, получено: %d", steps)
+	}
+
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		// При ошибке вернуть нули и ошибку.
+		return 0, 0, fmt.Errorf("не удалось преобразовать продолжительность: %w", err)
+	}
+
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		// В случае ошибки вывести её и вернуть пустую строку.
+		fmt.Printf("Ошибка при обработке данных: %v\n", err)
+		return ""
+	}
+
+	// Дистанция в метрах.
+	distanceMeters := float64(steps) * stepLength
+
+	// Дистанцию в километры.
+	distanceKm := distanceMeters / mInKm
+
+	// Вычисление количество калорий с помощью функции из другого пакета.
+	calories, err := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	if err != nil {
+		// Если при расчете калорий возникла ошибка
+		fmt.Printf("Ошибка при расчете калорий: %v\n", err)
+		return ""
+	}
+
+	result := fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
+		steps,
+		distanceKm,
+		calories,
+	)
+
+	return result
 }
